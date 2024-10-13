8dacc7d5f911e730d57bb1491a7597f344bd037e
ReginaLin2005 <ReginaLin2005@gmail.com>
2024-10-13 01:17:46 -0700

    set up flask project

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,9 +1,25 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import os
 
 app = Flask(__name__)
 
-@app.route('/upload', methods=['POST'])
+@app.route('/')
+def index():
+        return render_template('index.html')
+
+@app.route('/about')
+def about():
+        return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+        return render_template('contact.html')
+
+@app.route('/prediction')
+def predict():
+        return render_template('prediction.html')
+
+@app.route('/submit', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
         return jsonify({'error': 'no file part'}), 400
